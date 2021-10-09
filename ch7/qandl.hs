f5c2ea3d9b89dfcce6ea86f303192cf1533cecab
Reed O'Brien <reed@reedobrien.com>
2021-10-09 14:54:37 -0700

    chore: Add answers to questions

--- ch7/qandl.hs
+++ ch7/qandl.hs
@@ -34,6 +34,11 @@ myTail (_:xs) = xs
 -- The tail function in Haskell returns an error when called on an empty list.
 -- Modify myTail so that it does handle the case of an empty list by returning the
 -- empty list.
+myTail (_:xs) = xs
+myTail [] = error "tail called on an empty list"
 
 -- Q 7.2
 -- Rewrite myGCD by using pattern matching.
+myGCD' a 0 = a
+myGCD' a b = myGCD b $ mod a b
+
